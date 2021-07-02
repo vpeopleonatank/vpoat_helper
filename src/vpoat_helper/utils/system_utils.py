64895cd2c2bcc0fcb1b5d1b7fa3bcbf3f804427f
vpoat <vpoat@pop-os.localdomain>
2021-07-02 09:22:59 +0700

    add open_image_by_viewer

---
+++ src/vpoat_helper/utils/system_utils.py
@@ -0,0 +1,8 @@
+import sys
+import subprocess
+
+def open_image_by_viewer(path):
+    imageViewerFromCommandLine = {'linux':'xdg-open',
+                                  'win32':'explorer',
+                                  'darwin':'open'}[sys.platform]
+    subprocess.run([imageViewerFromCommandLine, path])
